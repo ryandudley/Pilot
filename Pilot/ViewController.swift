94a8f1200337cf20b8338c14c937e73bf9751bb3
Ryan Dudley <crdudley@gmail.com>
2016-04-10 12:11:09 -0700

    First project commit

---
+++ Pilot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pilot
+//
+//  Created by Ryan Dudley on 4/10/16.
+//  Copyright © 2016 Ryan Dudley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+
+
+platform :ios, '9.0'
+use_frameworks!
+
+target 'Pilot' do
+    pod 'Alamofire', '~> 3.3.1'
+    pod 'ReactiveCocoa', '~> 4.1'
+end
+
